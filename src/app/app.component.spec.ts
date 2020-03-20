c070ed11962d3c391cd3c7dffcb36278fdaae0b5
Orlando Burli Junior <orlando.burli@gmail.com>
2020-03-20 11:03:56 -0400

    Inicial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'entrega-sos-front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('entrega-sos-front');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('entrega-sos-front app is running!');
-  });
 });
