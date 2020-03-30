e334a5e8234b3a8daa945e24c451d163a93e4a2f
Orlando Burli Junior <orlando.burli@gmail.com>
2020-03-29 21:16:05 -0400

    Remoção de arquivos do docker

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/entrega-sos-front'));
 
 app.get('/*', function (req, res) {
-
   res.sendFile(path.join(__dirname + '/dist/entrega-sos-front/index.html'));
 });
 
